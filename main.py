b03e1e3e0dbbce0efca05272470631367ea42c52
Joshua Noble <121055115+BigJish@users.noreply.github.com>
2023-01-23 20:57:13 +0000

    added basic classes and sprites

--- main.py
+++ main.py
@@ -1,12 +1,79 @@
-import pygame, sys
-from pygame.locals import QUIT
+from pygame import *
 
-pygame.init()
-DISPLAYSURF = pygame.display.set_mode((400, 300))
-pygame.display.set_caption('Hello World!')
-while True:
-    for event in pygame.event.get():
-        if event.type == QUIT:
-            pygame.quit()
-            sys.exit()
-    pygame.display.update()
+init()
+clock = time.Clock()
+WIN = display.set_mode((500, 300))
+
+
+class Main:
+
+    def __init__(self):
+        self.game = Game()
+
+    def run(self):
+        while True:
+            for i in event.get():
+                if i.type == QUIT:
+                    quit()
+
+            self.game.run()
+
+            clock.tick(60)
+            display.update()
+
+
+class Game:
+
+    def __init__(self):
+        self.car = Car_go_nyoom()
+        self.bg = Background()
+
+    def run(self):
+        WIN.fill((100, 160, 230))
+        self.bg.draw()
+        self.car.draw()
+        self.car.move()
+
+
+class Car_go_nyoom:
+
+    def __init__(self):
+        self.image1 = image.load("SophieCar.png").convert_alpha()
+        self.image = self.image1
+        self.rect = Rect(200, 100, 50, 20)
+        self.speed = 1
+
+    def draw(self):
+        WIN.blit(self.image, (self.rect.x, self.rect.y))
+
+    def move(self):
+        k = key.get_pressed()
+
+        if k[K_w]:
+            self.rect.y -= self.speed
+            self.image = transform.rotate(self.image1, 90)
+
+        if k[K_s]:
+            self.rect.y += self.speed
+            self.image = transform.rotate(self.image1, -90)
+
+        if k[K_a]:
+            self.rect.x -= self.speed
+            self.image = transform.rotate(self.image1, 180)
+
+        if k[K_d]:
+            self.rect.x += self.speed
+            self.image = self.image1
+
+
+class Background:
+
+    def __init__(self):
+        pass
+
+    def draw(self):
+        draw.rect(WIN, (79, 82, 82), (50, 50, 400, 200), 50)
+
+
+main = Main()
+main.run()
